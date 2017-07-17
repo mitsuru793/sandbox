942b8ad65ba5d949333f29fbe3fddd96931b3aab
mitsuru793 <mitsuru793@gmail.com>
2017-07-17 16:58:03 +0900

    add php/2017-07-14_make_ternary_operator_method_chain.php

---
+++ php/2017-07-14_make_ternary_operator_method_chain.php
@@ -0,0 +1,34 @@
+<?php
+/*
+三項演算子の戻り値にメソッドチェーンにする。
+()で囲めばwithは必要ないが、読みやすさを考慮しているかもしれない。
+*/
+
+// https://github.com/tightenco/collect/blob/master/src/Illuminate/Support/helpers.php
+function with($object)
+{
+    return $object;
+}
+
+class Person
+{
+    public $name;
+
+    public function __construct(string $name)
+    {
+        $this->name = $name;
+    }
+
+    public function name() : string
+    {
+        return $this->name;
+    }
+}
+
+$res = with(true ? new Person('mike') : new Person('jane'))
+    ->name();
+assert($res === 'mike');
+
+$res = (false ? new Person('mike') : new Person('jane'))
+    ->name();
+assert($res === 'jane');
