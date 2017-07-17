6800c8420a6a720ce2d3b99f2a7f6f2332cbee45
mitsuru793 <mitsuru793@gmail.com>
2017-07-17 16:57:34 +0900

    add php/2017-07-13_use_interface_instead_of_child_class.php

---
+++ php/2017-07-13_use_interface_instead_of_child_class.php
@@ -0,0 +1,33 @@
+<?php
+/*
+タイプヒントに使われている型の子クラスは許容できない。インターフェースを使う。
+*/
+
+class P {}
+class C extends P{}
+
+class Text
+{
+    public function render(P $arg) {}
+
+    public static function make() : self
+    {
+        return new Text;
+    }
+}
+class Name extends Text
+{
+    // タイプヒントでは子クラスを許容できない。
+    // インターフェースを使う必要がある。
+    // public function render(C $arg) {}
+
+    public function render(P $arg) {}
+
+    // selfはNameを指すので駄目
+    // public static function make() : self
+
+    public static function make() : Text
+    {
+        return new Text;
+    }
+}
