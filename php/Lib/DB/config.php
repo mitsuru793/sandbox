51612df254c945a0d7293aa9d7288f78edb72aba
mitsuru793 <mitsuru793@gmail.com>
2017-07-19 00:43:03 +0900

    add php/Lib/DB/config.php

---
+++ php/Lib/DB/config.php
@@ -0,0 +1,12 @@
+<?php
+return [
+    'driver'    => 'mysql',
+    'host'      => '127.0.0.1',
+    'database'  => 'sandbox',
+    'username'  => 'root',
+    'password'  => '',
+    'port'      => '3306',
+    'charset'   => 'utf8',
+    'collation' => 'utf8_unicode_ci',
+    'prefix'    => '',
+];
