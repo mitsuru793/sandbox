d2d2a13b83fe84b2c30eefc2f1a73dd5aeb0496f
mitsuru793 <mitsuru793@gmail.com>
2017-07-29 01:13:19 +0900

    remove check deal if function has already defined

--- php/Lib/functions.php
+++ php/Lib/functions.php
@@ -3,27 +3,22 @@ namespace Lib;
 
 use Lib\Stringy;
 
-if (!function_exists('Lib\create')) {
-    function create($str, $encoding = null)
-    {
-        return new Stringy($str, $encoding);
-    }
+function create($str, $encoding = null)
+{
+    return new Stringy($str, $encoding);
 }
 
-if (!function_exists('Lib\puts')) {
-    function puts(string $str, $level = 0, $indent = '    ')
-    {
-        $out = '';
-        $lines = explode(PHP_EOL, $str);
-        foreach ($lines as $line) {
-            if ($level > 0) {
-                for ($i = 0; $i < $level; $i++) {
-                    $out .= $indent;
-                }
+function puts(string $str, $level = 0, $indent = '    ')
+{
+    $out = '';
+    $lines = explode(PHP_EOL, $str);
+    foreach ($lines as $line) {
+        if ($level > 0) {
+            for ($i = 0; $i < $level; $i++) {
+                $out .= $indent;
             }
-            $out .= $line . PHP_EOL;
         }
-        echo $out;
+        $out .= $line . PHP_EOL;
     }
+    echo $out;
 }
-
